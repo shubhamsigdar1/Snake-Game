5dbc238f1cdfb96d4d423cf353892d8fc3437bcb
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-12-16 20:03:06 +0530

    created movement animation

--- snake.game.js
+++ snake.game.js
@@ -56,3 +56,64 @@ const changeDirection = (newDirectionCode) => {
 
 //set starting point for snake on load
 let currentHeadPosition = TOTAL_PIXEL_COUNT / 2;
+
+//set initial length
+let snakeLength = 200;
+
+//start moving snak, wrap around to other side of screen if needed
+const moveSnake = () => {
+  switch (snakeCurrentDirection) {
+    case LEFT_DIR:
+      --currentHeadPosition;
+      const isHeadAtLeft =
+        currentHeadPosition % LINE_PIXEL_COUNT == LINE_PIXEL_COUNT - 1 ||
+        currentHeadPosition < 0;
+      if (isHeadAtLeft) {
+        currentHeadPosition += LINE_PIXEL_COUNT;
+      }
+      break;
+    case RIGHT_DIR:
+      ++currentHeadPosition;
+      const isHeadAtRight = currentHeadPosition % LINE_PIXEL_COUNT === 0;
+      if (isHeadAtRight) {
+        currentHeadPosition = currentHeadPosition - LINE_PIXEL_COUNT;
+      }
+      break;
+    case UP_DIR:
+      currentHeadPosition = currentHeadPosition - LINE_PIXEL_COUNT;
+      const isHeadAtTop = currentHeadPosition < 0;
+      if (isHeadAtTop) {
+        currentHeadPosition = currentHeadPosition + TOTAL_PIXEL_COUNT;
+      }
+      break;
+    case DOWN_DIR:
+      currentHeadPosition = currentHeadPosition + LINE_PIXEL_COUNT;
+      const isHeadAtBottom = currentHeadPosition > TOTAL_PIXEL_COUNT - 1;
+      if (isHeadAtBottom) {
+        currentHeadPosition = currentHeadPosition - TOTAL_PIXEL_COUNT;
+      }
+      break;
+    default:
+      break;
+  }
+
+  //access the correct pixel within the HTML collection
+  let nextSnakeHeadPixel = gameBoardPixel[currentHeadPosition];
+
+  //check if snake head is about to intersect with its own body
+  if (nextSnakeHeadPixel.classList.contains('snakeBodyPixel')) {
+    clearInterval(moveSnakeInterval);
+    alert(
+      `You have eaten ${totalFoodEaten} food and traveled ${totalDistanceTraveled} blocks.`
+    );
+    window.location.reload();
+  }
+
+  //Assuming empty pixel, add snake body styling
+  nextSnakeHeadPixel.classList.add('snakeBodyPixel');
+};
+
+createGameBoardPixels();
+createFood();
+
+let moveSnakeInterval = setInterval(moveSnake, 100);
